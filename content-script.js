e7716cd97d5630deeb465b86fcda132f0d01fbb0
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2022-11-23 19:05:56 -0500

    Ported to firefox

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
 try {
-    document.querySelectorAll("a[href='https://umass.moonami.com/login/index.php']")[0].click();
-  }
-  catch(e) {
-    console.log("logged in");
-  }
+  document.querySelectorAll("a[href='https://umass.moonami.com/login/index.php']")[0].click();
+}
+catch (e) {
+  console.log("logged in");
+}
 
